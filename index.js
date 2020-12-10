8fe56316b931d9929a2041ea125df3dee31e0af4
JNickBaughan <JNickBaughan@gmail.com>
2020-12-10 18:28:53 -0500

    add temporary under construction label

--- index.js
+++ index.js
@@ -11,21 +11,50 @@ const middlewares = [
 
 server.use(middlewares);
 
-server.get("/", (_, res) =>
-  res.send(`<!DOCTYPE html>
-            <style>
-            html,body{ width: 98vw; height: 96vh;  }
-            #root{ height: 100%; }
-            </style>
-            <html>
-              <head></head>
-              <body>
-              <div id="root" />
-                <script src="/bundle.js"></script>
-              </body>
-            </html>
-          `)
-);
+server.get("/", (_, res) => {
+  if (PORT !== 3000) {
+    return res.send(`<!DOCTYPE html>
+                      <style>
+                      html,body{ width: 98vw; height: 96vh;  }
+                      #root{ height: 100%; }
+                      #underConstruction{
+                        width: 500px;
+                        top: 200px;
+                        text-align: center;
+                        position: absolute;
+                        border: solid 5px black;
+                        background-color: yellow;
+                        height: 44px;
+                        margin: auto;
+                        z-index: 242;
+                        line-height: 41px;
+                        transform: rotate(20deg);
+                      }
+                      </style>
+                      <html>
+                        <head></head>
+                        <body>
+                        <div id="underConstruction">THIS SITE IS UNDER MAINTENANCE</div>
+                        <div id="root" />
+                          <script src="/bundle.js"></script>
+                        </body>
+                      </html>
+                    `);
+  }
+  return res.send(`<!DOCTYPE html>
+  <style>
+  html,body{ width: 98vw; height: 96vh;  }
+  #root{ height: 100%; }
+  </style>
+  <html>
+    <head></head>
+    <body>
+    <div id="root" />
+      <script src="/bundle.js"></script>
+    </body>
+  </html>
+`);
+});
 
 server.listen(PORT, function () {
   console.log(`server listening on port: ${PORT}`);
