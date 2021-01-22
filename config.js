5d74e09a69637b4a11c4504494f0c7820140492e
JNickBaughan <JNickBaughan@gmail.com>
2021-01-21 19:07:36 -0500

    fix image

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   bio: {
     name: "[ J. Nick Baughan ]",
-    photoUrl: "/me.png",
+    photoUrl: "/me.PNG",
     title: "Front-end developer working with Costar Group",
     blurb: "Well versed in Javascript. Working with React daily."
   },
